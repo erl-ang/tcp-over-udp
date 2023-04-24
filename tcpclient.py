2cfb405c1187aff746503e720f251883b069b6a8
Erin Liang <ell2147@columbia.edu>
2023-04-24 16:44:29 -0400

    proxy setup done! need to log still

--- tcpclient.py
+++ tcpclient.py
@@ -7,19 +7,20 @@ class SimplexTCPClient:
     """
     def __init__(self, file, address_of_udpl, port_number_of_udpl, windowsize, ack_port_number):
         self.file = file
-        self.address_of_udpl = address_of_udpl
-        self.port_number_of_udpl = port_number_of_udpl
+        self.proxy_address = (address_of_udpl, port_number_of_udpl)
         self.windowsize = windowsize
         self.ack_port_number = ack_port_number
         
         # Create a UDP socket using IPv4
-        self.socket = self.create_socket()
+        self.socket = self.create_and_bind_socket()
+        self.socket_num = 12345 # TODO change this later so there's no conflicts between port nums
     
-    def create_socket(self):
+    def create_and_bind_socket(self):
         """
         Create a UDP socket using IPv4
         """
         self.socket = socket(AF_INET, SOCK_DGRAM)
+        self.socket.bind(('', self.ack_port_number))
         return self.socket
     
     def get_data(self):
@@ -27,7 +28,7 @@ class SimplexTCPClient:
         For now, data will be a string literal. Later have to read file contents
         to send to the server.
         """
-        return input('Input message here')
+        return input('Input message here: ')
         
     
     def read_datagram(self):
@@ -43,13 +44,8 @@ class SimplexTCPClient:
         """
         message = self.get_data()
         
-        proxy_name = self.address_of_udpl
-        proxy_port = self.port_number_of_udpl
-        print(f"type of port: {type(proxy_port)}")
-        print(f"type of name {type(proxy_name)}")
-        
         # Attach destination address to the message.
-        self.socket.sendto(message.encode(), (proxy_name, proxy_port))
+        self.socket.sendto(message.encode(), self.proxy_address)
         modified_message, server_address = self.socket.recvfrom(2048)
         print(modified_message.decode())
         self.socket.close()
